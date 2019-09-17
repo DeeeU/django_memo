9f51c30cdcd66310cd75020fc0bffe75bf3bf4ad
Muragaruhae <kanabuuch@icloud.com>
2019-09-17 23:46:06 +0900

    user追加

--- memo/blog/urls.py
+++ memo/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'blog'
 urlpatterns = [
@@ -8,4 +9,6 @@ urlpatterns = [
     path('new_memo', views.new_memo, name='new_memo'),
     path('delete_memo/<int:memo_id>', views.delete_memo, name='delete_memo'),
     path('edit_memo/<int:memo_id>', views.edit_memo, name='edit_memo'),
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     ]
